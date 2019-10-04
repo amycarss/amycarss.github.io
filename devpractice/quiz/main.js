bf996db5f7b1d8add307199e6ae52ebd270ac534
Amy Whitesall <amycarss@umich.edu>
2019-10-04 16:52:38 -0400

    add directory of files for simple quiz

---
+++ devpractice/quiz/main.js
@@ -0,0 +1,18 @@
+function submit(){
+ window.location("response.html");  
+}
+
+function check(){
+        
+    var adjuncts = document.getElementById("adjuncts").checked;
+    //var lecturers = document.getElementById("lecturers")
+   // var postdocs = document.getElementById("postdocs")
+    //var profPractice = document.getElementById("prof-practice") 
+    //var researchers = docment.getElementById("researchers")
+    //var tenureTrack = document.getElementById("tenure-track")
+     
+   
+    if (adjuncts = false){
+            console.log("booyah!")
+    }
+}
