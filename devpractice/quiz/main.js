4af0a1a8a50159bce1da9df7af1770531d5f5228
Amy Whitesall <amycarss@umich.edu>
2019-10-09 11:38:35 -0400

    updates layout, moves response content to index

--- devpractice/quiz/main.js
+++ devpractice/quiz/main.js
@@ -1,18 +1,5 @@
-function submit(){
- window.location("response.html");  
-}
 
-function check(){
-        
-    var adjuncts = document.getElementById("adjuncts").checked;
-    //var lecturers = document.getElementById("lecturers")
-   // var postdocs = document.getElementById("postdocs")
-    //var profPractice = document.getElementById("prof-practice") 
-    //var researchers = docment.getElementById("researchers")
-    //var tenureTrack = document.getElementById("tenure-track")
-     
-   
-    if (adjuncts = false){
-            console.log("booyah!")
-    }
+function response(){
+    document.getElementById("explain").style.visibility = "visible"; //make the explainer visible only after quiz is submitted
+       
 }
