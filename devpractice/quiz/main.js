1e671f7d2c8a02178359db1e50201adab070c6e1
Amy Whitesall <amycarss@umich.edu>
2019-10-09 12:41:44 -0400

    fixes disappearing explainer problem

--- devpractice/quiz/main.js
+++ devpractice/quiz/main.js
@@ -1,5 +1,6 @@
 
 function response(){
+    console.log("response is firing");
     document.getElementById("explain").style.visibility = "visible"; //make the explainer visible only after quiz is submitted
-       
+    console.log("and it got to the second log");
 }
