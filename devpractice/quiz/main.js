d971699e3f66011e01c0eaca46b1e4c5ae29912d
Amy Whitesall <amycarss@umich.edu>
2019-10-09 12:50:44 -0400

    fixed type for submit button

--- devpractice/quiz/main.js
+++ devpractice/quiz/main.js
@@ -1,6 +1,4 @@
 
 function response(){
-    console.log("response is firing");
     document.getElementById("explain").style.visibility = "visible"; //make the explainer visible only after quiz is submitted
-    console.log("and it got to the second log");
 }
