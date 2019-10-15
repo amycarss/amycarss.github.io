ed6315cb7b9d5391b90933b57eea0626ca42f3b5
Amy Whitesall <amycarss@umich.edu>
2019-10-14 20:34:16 -0400

    update checker function

--- devpractice/quiz/main.js
+++ devpractice/quiz/main.js
@@ -1,4 +1,32 @@
-
+/* 
 function response(){
     document.getElementById("explain").style.visibility = "visible"; //make the explainer visible only after quiz is submitted
+} */
+function checker(){
+    var adjuncts = document.quiz.adjuncts.value;
+    var lecturers = document.quiz.lecturers.value;
+    var postdocs = document.quiz.postdocs.value;
+    var profPractice =document.quiz.profpractice.value;
+    var researchers = document.quiz.researchers.value;
+    var tenureTrack = document.quiz.tenuretrack.value;
+    var counter = 0;
+    if (lecturers.checked){
+        counter++;
 }
+    console.log(counter);
+    if (profPractice.checked){
+        counter++;
+}
+    console.log(counter);
+    if (researchers.checked){
+        counter++;
+}
+    console.log(counter);
+    if (tenureTrack.checked){
+        counter++;
+}
+    console.log(counter);
+    document.getElementById("explain").style.visibility = "visible"; //make the explainer visible only after quiz is submitted
+    document.getElementById("result").innerHTML = "You got " + counter + " of 4 correct.";
+}
+
