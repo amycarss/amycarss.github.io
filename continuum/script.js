55ce64d1627fc240fae5a4e60d82021c4876e35f
Robert Connot <rmconnot@Roberts-MacBook-Pro.local>
2020-07-16 16:03:35 -0600

    submission screen lock

--- continuum/script.js
+++ continuum/script.js
@@ -1,9 +1,11 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbw-x2gHmWccuxvivJPRKap6N25vPFTWvgjDiOzvWwXPHvZ6-Gc/exec"
 const form = document.forms["in_tension_survey"]
+
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response),
+    document.getElementById("submission_lock").style.display = "block")
     .then(() => window.location.replace("success_page.html"))
     .catch(error => console.error('Error!', error.message))
 });
